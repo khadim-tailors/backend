9a5cf058313a78f7e0d90be996f8566d760f00b4
Salman <salmanraza1993@gmail.com>
2021-10-05 23:44:45 +0400

    employee login and change passowrd etc

--- api/employees.js
+++ api/employees.js
@@ -4,9 +4,25 @@ const admin = require("firebase-admin");
 const { sendResponse } = require("../helper/response.helper");
 const employeeRef = admin.firestore().collection("employees");
 const bcrypt = require('bcrypt');
+const { getUserWithUserId, getEmployeeByUsername } = require("../helper/helper");
 const saltRounds = 2;
 employees.post("/getAllEmployees", async (req, res) => {
-    const { };
+    const { user_id } = req.body;
+    try {
+        const Allemployees = [];
+        const empList = await employeeRef.where("user_id", "==", user_id).get();
+        empList.forEach(doc => {
+            const emp = {
+                employee_id: doc.id,
+                ...doc.data()
+            };
+            delete emp.password;
+            Allemployees.push(emp);
+        });
+        sendResponse({ res, message: "Employees fetched successfully!", status: true, result: Allemployees });
+    } catch (err) {
+        sendResponse({ res, message: err.message ? err.message : "Employees fetched failed!", status: false, result: [] });
+    }
 });
 
 async function isUsernameAlreadyExist(username) {
@@ -20,7 +36,6 @@ async function isUsernameAlreadyExist(username) {
     }
 }
 
-
 async function isEmployeeAlreadyExist(phone) {
     const allEmployees = [];
     const employee = await employeeRef.where("phone", "==", phone).get();
@@ -28,6 +43,7 @@ async function isEmployeeAlreadyExist(phone) {
     if (allEmployees.length > 0) return true;
     return false;
 }
+
 employees.post("/addNewEmployee", async (req, res) => {
     const data = req.body;
     const { user_id, username, password, phone } = data;
@@ -56,5 +72,70 @@ employees.post("/addNewEmployee", async (req, res) => {
 
 });
 
-employees.post("");
+employees.post("/employeeLogin", async (req, res) => {
+    const { username, password } = req.body;
+    if (username && password) {
+        try {
+            const employeeData = await getEmployeeByUsername(username);
+            if (employeeData) {
+                const hashedPassword = employeeData.password;
+                delete employeeData.password;
+                bcrypt.compare(password, hashedPassword, async (err, result) => {
+                    // result == true then the password is matched
+                    if (result) {
+                        console.log(employeeData.user_id);
+                        const orgData = await getUserWithUserId(employeeData.user_id);
+                        if (orgData) return sendResponse({
+                            res,
+                            message: "Logged in successful!",
+                            status: true,
+                            result: { ...employeeData, organization_name: orgData.first_name + " " + orgData.last_name }
+                        });
+                        else return sendResponse({ res, message: "Organisation not found", status: false, result: [] });
+                    } else return sendResponse({
+                        res,
+                        message: "You have entered wrong password, if you forget your password contact your admin to change it.",
+                        status: false,
+                        result: []
+                    });
+                });
+            } else return sendResponse({ res, message: "username does not exist, make sure to pass correct username", status: false, result: [] });
+        } catch (error) {
+
+        }
+    } else return sendResponse({ res, message: "username and password is required!", status: false, result: [] });
+});
+
+employees.post("/changeEmployeePassword", async (req, res) => {
+    const { username, old_password, new_password, user_id } = req.body;
+    if (username && old_password && new_password && user_id) {
+        const employeeData = await getEmployeeByUsername(username);
+        if (employeeData) {
+            //if username in the employee details and the one which is send in the request is not same then
+            // the employee is not belongs to the organization
+            if (employeeData.user_id !== user_id) return sendResponse({
+                res,
+                message: "Employee does not belongs to you. You can only change your employee data.",
+                status: false,
+                result: []
+            });
+            bcrypt.compare(old_password, employeeData.password, async (err, result) => {
+                // result == true password matches
+                if (result) {
+                    bcrypt.genSalt(saltRounds, async (err, salt) => {
+                        bcrypt.hash(new_password, salt, (err, hash) => {
+                            if (err) return sendResponse({ res, message: err.message ? err.message : "password encryption failed", status: false, result: [] });
+                            employeeRef.doc(username).update({ password: hash }).then(response => {
+                                return sendResponse({ res, message: "password updated successfully", result: [], status: true });
+                            }).catch(err => {
+                                return sendResponse({ res, message: err.message ? err.message : "Something went wrong while writing in the database.", status: false, result: [] });
+                            });
+                        });
+                    });
+                }
+            });
+        } else return sendResponse({ res, status: false, message: "employee not found, make sure you pass the correct username", result: [] });
+    } else return sendResponse({ res, message: "username, old password, new password and user_id is mandatory", status: false, result: [] });
+});
+
 module.exports = employees;
---
+++ helper/helper.js
@@ -0,0 +1,15 @@
+const admin = require("firebase-admin");
+const userRef = admin.firestore().collection("users");
+const employeeRef = admin.firestore().collection("employees");
+
+module.exports.getUserWithUserId = async (user_id) => {
+    const user = await userRef.doc(user_id).get();
+    if (user.exists) return { user_id: user.id, ...user.data() };
+    return false;
+};
+
+module.exports.getEmployeeByUsername = async (username) => {
+    const employee = await employeeRef.doc(username).get();
+    if (employee.exists) return { employee_id: employee.id, ...employee.data() };
+    else return false;
+};
