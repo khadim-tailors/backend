302ddf5f818cada83865f2bf598204ed45167987
Salman <salmanraza1993@gmail.com>
2021-10-21 11:38:07 +0400

    users api separated.

---
+++ api/users.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const users = express.Router();
+const cors = require('cors');
+const admin = require("firebase-admin");
+const moment = require("moment");
+users.use(cors({ origin: true }));
+const { sendResponse } = require("../helper/response.helper");
+const usersRef = admin.firestore().collection('users');
+
+users.post("/createNewUser", async (req, res) => {
+  const { user_id } = req.body;
+  try {
+    admin
+      .auth()
+      .getUser(user_id)
+      .then(async (userRecord) => {
+        // See the UserRecord reference doc for the contents of userRecord.
+        const user = {
+          email: userRecord.email,
+          emailVerified: userRecord.emailVerified,
+          disabled: userRecord.disabled,
+          creationTime: moment(userRecord.metadata.creationTime).format("DD-MMM-YYYY HH:mm:ss"),
+          phone: userRecord.phone,
+          is_profile_filled: false,
+          plan: {
+            name: 'Free',
+            order_used: 0,
+            end_date: moment().add(1, 'M').format('DD-MM-YYYY HH:mm:ss'),
+            star_date: moment().format("DD-MM-YYYY HH:mm:ss"),
+            order_left: 10,
+            total_order_count: 10,
+            order_used: 0
+
+          }
+        };
+        if (!user.email) delete user.email;
+        if (!user.phone) delete user.phone;
+        console.log(user);
+        const snapshot = await admin.firestore().collection('users').doc(user_id).set(user);
+        return sendResponse({ res, message: "User creation successful.", result: user, status: true });
+      })
+      .catch((error) => {
+        return sendResponse({ res, message: error.message ? error.message : "Something went wrong", result: [], status: false });
+      });
+  } catch (error) {
+    sendResponse({ res, message: error.message ? error.message : "Failed to create a new user", status: false, result: [] });
+  }
+
+});
+
+users.post("/getUser", async (req, res) => {
+  const { user_id } = req.body;
+  try {
+    const snapshot = await admin.firestore().collection('users').doc(user_id).get();
+    return sendResponse({ res, message: "User fetched successfully.", status: true, result: { user_id: snapshot.id, ...snapshot.data() } });
+  } catch (error) {
+    return sendResponse({ res, message: error.message ? error.message : "Failed to get the user details.", result: [], status: false, });
+  }
+});
+
+users.post("/updateUser", async (req, res) => {
+  const { body } = req;
+  try {
+    const response = await admin.firestore().collection("users").doc(body.user_id).update(body);
+    sendResponse({ res, message: "User updated successfully.", result: response, status: true, });
+  } catch (error) {
+    return sendResponse({ res, message: error.message ? error.message : "User update failed.", result: [], status: false, });
+
+  }
+});
+
+users.get("/getAllUsers", async (req, res) => {
+  try {
+    const allUsers = [];
+    const users = await usersRef.get();
+    users.forEach(user => {
+      allUsers.push({ user_id: user.id, ...user.data() });
+      return sendResponse({ res, message: "Users fetched successfully.", status: true, result: allUsers });
+    });
+  } catch (err) {
+    return sendResponse({ res, message: "Users fetch failed.", status: false, result: [] });
+  }
+});
+
+module.exports = users;
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const functions = require("firebase-functions");
-const moment = require("moment");
-const express = require("express");
-const app = express();
 const admin = require("firebase-admin");
 admin.initializeApp();
 const profiles = require("./api/profile");
@@ -11,69 +8,9 @@ const service = require("./admin/services");
 const plan = require("./admin/plans");
 const shop = require("./admin/shops");
 const dashboard = require("./admin/dashboard");
-const { sendResponse } = require("./helper/response.helper");
+const user = require("./api/users");
 
-app.post("/createNewUser", async (req, res) => {
-  const { user_id } = req.body;
-  try {
-    admin
-      .auth()
-      .getUser(user_id)
-      .then(async (userRecord) => {
-        // See the UserRecord reference doc for the contents of userRecord.
-        const user = {
-          email: userRecord.email,
-          emailVerified: userRecord.emailVerified,
-          disabled: userRecord.disabled,
-          creationTime: moment(userRecord.metadata.creationTime).format("DD-MMM-YYYY HH:mm:ss"),
-          phone: userRecord.phone,
-          plan:{
-            name:'Free',
-            order_used:0,
-            end_date:moment().add(1, 'M').format('DD-MM-YYYY HH:mm:ss'),
-            star_date:moment().format("DD-MM-YYYY HH:mm:ss"),
-            order_left:10,
-            total_order_count:10,
-            order_used:0
-        
-          }
-        };
-        if (!user.email) delete user.email;
-        if (!user.phone) delete user.phone;
-        console.log(user);
-        const snapshot = await admin.firestore().collection('users').doc(user_id).set(user);
-        return sendResponse({ res, message: "User creation successful.", result: user, status: true });
-      })
-      .catch((error) => {
-        return sendResponse({ res, message: error.message ? error.message : "Something went wrong", result: [], status: false });
-      });
-  } catch (error) {
-    sendResponse({ res, message: error.message ? error.message : "Failed to create a new user", status: false, result: [] });
-  }
-
-});
-app.post("/getUser", async (req, res) => {
-  const { user_id } = req.body;
-  try {
-    const snapshot = await admin.firestore().collection('users').doc(user_id).get();
-    return sendResponse({ res, message: "User fetched successfully.", status: true, result: { user_id: snapshot.id, ...snapshot.data() } });
-  } catch (error) {
-    return sendResponse({ res, message: error.message ? error.message : "Failed to get the user details.", result: [], status: false, });
-  }
-});
-
-app.post("/updateUser", async (req, res) => {
-  const { body } = req;
-  try {
-    const response = await admin.firestore().collection("users").doc(body.user_id).update(body);
-    sendResponse({ res, message: "User updated successfully.", result: response, status: true, });
-  } catch (error) {
-    return sendResponse({ res, message: error.message ? error.message : "User update failed.", result: [], status: false, });
-
-  }
-});
-
-exports.users = functions.https.onRequest(app);
+exports.users = functions.https.onRequest(user);
 exports.profile = functions.https.onRequest(profiles);
 exports.customers = functions.https.onRequest(customer);
 exports.employees = functions.https.onRequest(employee);
